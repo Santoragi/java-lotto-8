41ebdb2ad034d2927f6923bde494bdd4102bc920
Santoragi <woojj1254577@naver.com>
2025-11-01 13:34:38 +0900

    feat(InputView): 당첨 번호 입력 기능
    사용자에게 당첨 번호를 입력받는 기능을 추가
    ConsoleInputView
    - getWinningNumbers(): 사용자에게 당첨 번호를 입력받는 메소드

--- src/main/java/lotto/view/ConsoleInputView.java
+++ src/main/java/lotto/view/ConsoleInputView.java
@@ -5,10 +5,17 @@ import camp.nextstep.edu.missionutils.Console;
 public class ConsoleInputView implements InputView {
 
     private static final String PRICE_MESSAGE = "구입금액을 입력해 주세요.";
+    private static final String WINNING_NUMBER_MESSAGE = "당첨 번호를 입력해 주세요.";
 
     @Override
     public String getPrice() {
         System.out.println(PRICE_MESSAGE);
+        return Console.readLine();
+    }
+
+    @Override
+    public String getWinningNumbers() {
+        System.out.println(WINNING_NUMBER_MESSAGE);
         return Console.readLine();
     }
 }
--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -3,4 +3,5 @@ package lotto.view;
 public interface InputView {
 
     String getPrice();
+    String getWinningNumbers();
 }
