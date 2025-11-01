99f976ccebef425efde353dca79119b2006d7bfa
Santoragi <woojj1254577@naver.com>
2025-11-01 14:42:38 +0900

    feat(BonusNumberValidator): 보너스 번호 검증 기능
    보너스 번호를 검증하는 기능 추가
    - validateDuplicate(): 입력한 당첨 번호와 보너스 번호가 중복되면 예외 처리
    - validateNumberRange(): 보너스 번호의 범위 검증
    - validateMinNumber(): 보너스 번호가 1미만이면 예외 처리
    - validateMaxNumber(): 보너스 번호가 45초과이면 예외 처리

---
+++ src/main/java/lotto/util/validator/BonusNumberValidator.java
@@ -0,0 +1,40 @@
+package lotto.util.validator;
+
+import static lotto.constant.LottoConstants.MAX_NUMBER;
+import static lotto.constant.LottoConstants.MIN_NUMBER;
+
+import java.util.List;
+
+public class BonusNumberValidator {
+
+    public void validate(List<Integer> winningNumbers, Integer bonusNumber) {
+        validateNumberRange(bonusNumber);
+        validateDuplicate(winningNumbers, bonusNumber);
+    }
+
+    private void validateDuplicate(List<Integer> winningNumbers, Integer bonusNumber) {
+        boolean isDuplicate = winningNumbers.stream()
+                .anyMatch( number -> number.equals(bonusNumber));
+
+        if(isDuplicate) {
+            throw new IllegalArgumentException("보너스 번호는 당첨 번호와 중복될 수 없습니다.");
+        }
+    }
+
+    private void validateNumberRange(Integer bonusNumber) {
+        validateMinNumber(bonusNumber);
+        validateMaxNumber(bonusNumber);
+    }
+
+    private void validateMinNumber(Integer bonusNumber) {
+        if(bonusNumber < MIN_NUMBER) {
+            throw new IllegalArgumentException("보너스 번호는 최소 1입니다.");
+        }
+    }
+
+    private void validateMaxNumber(Integer bonusNumber) {
+        if(bonusNumber > MAX_NUMBER) {
+            throw new IllegalArgumentException("보너스 번호는 최대 45입니다.");
+        }
+    }
+}
