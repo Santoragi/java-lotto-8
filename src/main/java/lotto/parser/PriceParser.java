628d9aa810cfec4ae2c003048cf3a7f48a3848fd
Santoragi <woojj1254577@naver.com>
2025-11-01 11:18:38 +0900

    feat(PriceParser): 구입 금액 변환 기능 추가
    PriceParser: 사용자가 입력한 구입 금액을 숫자로 변경 . 반환, 숫자 외의 입력은 예외 처리

---
+++ src/main/java/lotto/parser/PriceParser.java
@@ -0,0 +1,13 @@
+package lotto.parser;
+
+public class PriceParser {
+
+    public int parse(String input) {
+        try {
+            int price = Integer.parseInt(input);
+            return price;
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("구입 금액은 정수로 입력해야합니다.");
+        }
+    }
+}
