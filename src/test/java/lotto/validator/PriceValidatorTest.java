60805694911d96fce80deaafbb9a66115da63246
Santoragi <woojj1254577@naver.com>
2025-11-01 11:57:21 +0900

    test(PriceValidator): 구입 금액 검증 테스트
    - 최소값보다 작은 값에서 예외 발생
    - 최소값보다 큰 값에서 검증 성공
    - 최대값보다 작은 값에서 검증 성공
    - 최대값보다 큰 값에서 예외 발생
    - 구입 금액이 로또 한장의 가격으로 나누어 떨어지면 검증 성공
    - 구입 금액이 로또 한장의 가격으로 나누어 떨어지지 않으면 예외 발생

---
+++ src/test/java/lotto/validator/PriceValidatorTest.java
@@ -0,0 +1,78 @@
+package lotto.validator;
+
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class PriceValidatorTest {
+
+    private PriceValidator priceValidator;
+    @BeforeEach
+    void setUp() {
+        priceValidator = new PriceValidator();
+    }
+
+    @Test
+    @DisplayName("최소 구입 금액보다 적은 값에 대해 예외 처리")
+    void 최소_구입_금액_미만() {
+
+        int price = 10;
+
+        assertThatThrownBy(() -> priceValidator.validate(price))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("최소 구입 금액은 1000원입니다.");
+    }
+
+    @Test
+    @DisplayName("최소 구입 금액보다 큰 값에 대해 검증을 성공")
+    void 최소_구입_금액_이상() {
+
+        int price = 3000;
+
+        assertDoesNotThrow(() -> priceValidator.validate(price));
+    }
+
+    @Test
+    @DisplayName("최대 구입 금액보다 작은 값에 대해 검증을 성공")
+    void 최대_구입_금액_미만() {
+
+        int price = 10000;
+
+        assertDoesNotThrow(() -> priceValidator.validate(price));
+    }
+
+    @Test
+    @DisplayName("최대 구입 금액보다 큰 값에 대해 예외 처리")
+    void 최대_구입_금액_이상() {
+
+        int price = 200000;
+
+        assertThatThrownBy(() -> priceValidator.validate(price))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("로또는 최대 10만원까지 구매 가능합니다.");
+    }
+
+    @Test
+    @DisplayName("로또 한장의 가격으로 나누어 떨어지는 경우 검증을 성공")
+    void 구입_금액_1000원단위_만족() {
+
+        int price = 100000;
+
+        assertDoesNotThrow(() -> priceValidator.validate(price));
+    }
+
+    @Test
+    @DisplayName("로또 한장의 가격으로 나누어 떨어지지 않는 경우 예외 처리")
+    void 구입_금액_1000원단위_불만족() {
+
+        int price = 10001;
+
+        assertThatThrownBy(() -> priceValidator.validate(price))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("구매 금액은 로또 한 장의 가격(1000원)으로 나누어 떨어져야합니다.");
+    }
+}
