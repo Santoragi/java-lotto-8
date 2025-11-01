f79c4c5a8c8047dcca53ab58b3c6e535c789395c
Santoragi <woojj1254577@naver.com>
2025-11-01 11:45:27 +0900

    test(PriceParser): 구입 금액 변환 테스트 수정
    BeforeEach 어노테이션을 사용하여 PriceParser 객체를 셋업하여 반복되는 문장을 줄임

--- src/test/java/lotto/parser/PriceParserTest.java
+++ src/test/java/lotto/parser/PriceParserTest.java
@@ -3,16 +3,26 @@ package lotto.parser;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class PriceParserTest {
 
+    private PriceParser priceParser;
+
+    @BeforeEach
+    void setUp() {
+        priceParser = new PriceParser();
+    }
+
     @Test
     @DisplayName("구입 금액을 올바르게 숫자로 변환")
     void 구입_금액_숫자입력() {
-        PriceParser priceParser = new PriceParser();
-        int price = priceParser.parse("1000");
+
+        String input = "1000";
+
+        int price = priceParser.parse(input);
 
         assertThat(price).isEqualTo(1000);
     }
@@ -20,9 +30,10 @@ public class PriceParserTest {
     @Test
     @DisplayName("구입 금액이 숫자가 아닌 경우 예외 처리")
     void 구입_금액_문자입력() {
-        PriceParser priceParser = new PriceParser();
 
-        assertThatThrownBy(() -> priceParser.parse("10000원"))
+        String input = "10000원";
+
+        assertThatThrownBy(() -> priceParser.parse(input))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("구입 금액은 정수로 입력해야합니다.");
     }
