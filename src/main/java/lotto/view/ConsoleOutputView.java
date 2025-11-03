baab308151910778633db5f67e535005e57bea72
Santoragi <woojj1254577@naver.com>
2025-11-03 13:38:05 +0900

    feat(OutputView): 로또 결과 출력 기능
    각 등수별로 조건과 일치하는 로또의 개수를 출력하는 기능을 추가

--- src/main/java/lotto/view/ConsoleOutputView.java
+++ src/main/java/lotto/view/ConsoleOutputView.java
@@ -1,11 +1,23 @@
 package lotto.view;
 
+import java.text.NumberFormat;
 import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+import lotto.constant.LottoRank;
 import lotto.domain.Lotto;
 
 public class ConsoleOutputView implements OutputView{
 
     private static final String LOTTO_COUNT_MESSAGE = "개를 구매했습니다.";
+    private static final String LOTTO_RESULT_MESSAGE = "당첨 통계\n---";
+    private static final LottoRank[] LOTTO_RESULT_ORDER = {
+            LottoRank.FIFTH,
+            LottoRank.FOURTH,
+            LottoRank.THIRD,
+            LottoRank.SECOND,
+            LottoRank.FIRST
+    };
 
     @Override
     public void printLottoCount(int count) {
@@ -16,5 +28,31 @@ public class ConsoleOutputView implements OutputView{
     @Override
     public void printLottoNumbers(List<Lotto> lottos) {
         lottos.forEach(lotto -> System.out.println(lotto.toString()));
+    }
+
+    @Override
+    public void printLottoResult(Map<LottoRank, Integer> lottoResult) {
+        System.out.println(LOTTO_RESULT_MESSAGE);
+        for(LottoRank rank : LOTTO_RESULT_ORDER) {
+            String rankMessage = getRankMessage(rank);
+            int count = lottoResult.getOrDefault(rank, 0);
+
+            System.out.println(rankMessage + " - " + count + "개");
+        }
+    }
+
+    private String getRankMessage(LottoRank rank) {
+        String matchMessage;
+        int matchCount = rank.getMatchCount();
+        boolean matchBonus = rank.getMatchBonus();
+        if (matchCount == 5 && matchBonus) {
+            matchMessage = matchCount + "개 일치, 보너스 볼 일치";
+        }
+        matchMessage = matchCount + "개 일치";
+
+        NumberFormat nf = NumberFormat.getInstance(Locale.KOREA);
+        String prizeMessage = " (" + nf.format(rank.getPrize()) + "원)";
+
+        return matchMessage + prizeMessage;
     }
 }
--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,10 +1,13 @@
 package lotto.view;
 
 import java.util.List;
+import java.util.Map;
+import lotto.constant.LottoRank;
 import lotto.domain.Lotto;
 
 public interface OutputView {
 
     void printLottoCount(int count);
     void printLottoNumbers(List<Lotto> lotto);
+    void printLottoResult(Map<LottoRank, Integer> lottoResult);
 }
